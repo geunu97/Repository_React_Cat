d04e8390c16f9cc2228e4ed547e6d45511140e21
olo90632951 <olo90632951@gmail.com>
2022-08-01 17:56:29 +0900

    useCallback 적용

--- src/components/records/Records.js
+++ src/components/records/Records.js
@@ -1,17 +1,21 @@
 import { getCats } from '../../apis/Api';
 import { Section, Card } from './Styles';
+import { useCallback } from 'react';
 
 const Records = ({ records, setCats, setLoading, setInput }) => {
-  const onClickRecords = async (item) => {
-    try {
-      setInput(item);
-      const response = await getCats(item, setLoading);
-      setCats(response);
-      localStorage.setItem('data', JSON.stringify(response));
-    } catch {
-      console.log('record 에러');
-    }
-  };
+  const onClickRecords = useCallback(
+    async (item) => {
+      try {
+        setInput(item);
+        const response = await getCats(item, setLoading);
+        setCats(response);
+        localStorage.setItem('data', JSON.stringify(response));
+      } catch {
+        console.log('record 에러');
+      }
+    },
+    [setCats, setInput, setLoading],
+  );
 
   return (
     <Section>
